95a06c0f7faa218fcf94f1133fc41e1df8279b51
DescaMr <drizal07@gmail.com>
2024-06-12 20:38:17 +0700

    my_order page (finish)

--- lib/page/my_order.dart
+++ lib/page/my_order.dart
@@ -1,33 +1,37 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TransactionHistoryPage(),
-    );
-  }
-}
+import 'package:google_fonts/google_fonts.dart';
+import 'package:online_app_final_project/component/list_colour.dart';
 
 class TransactionHistoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {},
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100.0),
+        child: Column(
+          children: [
+            AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {},
+              ),
+              backgroundColor: Colors.white,
+              elevation: 0,
+            ),
+            Text(
+              'My Order',
+              style: GoogleFonts.montserrat(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600),
+            ),
+          ],
         ),
-        title: Text('My Order'),
-        centerTitle: true,
       ),
       body: ListView.builder(
         padding: EdgeInsets.all(16.0),
-        itemCount: 5, // Replace with the number of transactions
+        itemCount: 5,
         itemBuilder: (context, index) {
           return TransactionCard();
         },
@@ -40,83 +44,132 @@ class TransactionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.white,
       margin: EdgeInsets.symmetric(vertical: 8.0),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12.0),
       ),
+      elevation: 5.0,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '7 June 2024',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-                  decoration: BoxDecoration(
-                    color: Colors.green[100],
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  child: Text(
-                    'Done',
-                    style: TextStyle(
-                        color: Colors.green, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
-            ),
+            _buildTimeAndStatus(),
+            Divider(),
             SizedBox(height: 8.0),
             Row(
               children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8.0),
-                  child: Image.network(
-                    'https://via.placeholder.com/50',
-                    height: 50,
-                    width: 50,
-                    fit: BoxFit.cover,
-                  ),
-                ),
+                _buildImage(),
                 SizedBox(width: 16.0),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Brown Suite',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 4.0),
-                      Text('10 pcs'),
-                      SizedBox(height: 4.0),
-                      Text('Rp 10.000.000',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      SizedBox(height: 4.0),
-                      Text('#TR2456DEDK', style: TextStyle(color: Colors.grey)),
-                    ],
-                  ),
-                ),
-                SizedBox(width: 16.0),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.brown,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
-                  child: Text('Buy Again'),
-                ),
+                _buildContent(),
               ],
             ),
+            _buildBottonId(),
           ],
         ),
       ),
+    );
+  }
+
+  Row _buildTimeAndStatus() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '7 June 2024',
+          style: GoogleFonts.montserrat(
+              color: greyPrimary, fontSize: 15, fontWeight: FontWeight.w600),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 2.0),
+          decoration: BoxDecoration(
+            color: Colors.green[100],
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+          child: Text(
+            'Done',
+            style: GoogleFonts.montserrat(
+                color: Colors.green, fontSize: 15, fontWeight: FontWeight.w600),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Positioned _buildImage() {
+    return Positioned(
+      bottom: 50,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Image.asset(
+          'assets/brown_jacket.jpg',
+          height: 65,
+          width: 65,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Expanded _buildContent() {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Brown Suite',
+            style: GoogleFonts.montserrat(
+                color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 1.0),
+          Text(
+            '10 pcs',
+            style: GoogleFonts.montserrat(
+                color: Colors.grey[600],
+                fontSize: 15,
+                fontWeight: FontWeight.w600),
+          ),
+          SizedBox(height: 1.0),
+          Text(
+            'Rp 10.000.000',
+            style: GoogleFonts.montserrat(
+              color: Colors.black,
+              fontSize: 15,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 1.0),
+        ],
+      ),
+    );
+  }
+
+  Row _buildBottonId() {
+    return Row(
+      children: [
+        Text('#TR2456DEDK', style: TextStyle(color: greyPrimary)),
+        SizedBox(width: 16.0),
+        Spacer(),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: brownSecondary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(6.0),
+            ),
+            padding: EdgeInsets.all(12.0),
+          ),
+          child: Text(
+            'Buy Again',
+            style: GoogleFonts.montserrat(
+              color: Colors.white,
+              fontSize: 17,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
